35de262a1c0961f084bc5c3844126c90cf369b21
SREERAM CHARAN <sreeramcharan202@gmail.com>
2026-07-26 13:11:46 +0530

    Time: 5 ms (80.93%), Space: 45.8 MB (87.72%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev =0;
+        int original = x;
+        while(x!=0){
+        int digit = x%10;
+        if(rev>Integer.MAX_VALUE/10||(rev == Integer.MAX_VALUE/10&& digit>7)){
+            return false;
+        }
+        rev = rev*10+digit;
+        x=x/10;
+        }
+         return original == rev;
+    }
+}
